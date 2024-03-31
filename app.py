d66193669e2c7950868b1b8d244a58ca501cd644
BenU <ngluonghungg2611@gmail.com>
2024-03-31 13:49:14 +0700

    fire and smoke

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from ultralytics import YOLO
 
+# Khoi tao FlaskPI server
 app = Flask(__name__ ,static_url_path='/static')
 model = YOLO('best.pt')
 
